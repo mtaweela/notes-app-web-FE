65002c034feb84ba0a561ef9274732f59f2e2590
mtaweela <mohamed.taweela56@gmail.com>
2018-08-06 21:42:12 +0200

    -- add the main structure of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { UserComponent } from './views/user/user.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { HeaderComponent } from './views/addons/header/header.component';
+import { FooterComponent } from './views/addons/footer/footer.component';
+import { SidebarComponent } from './views/addons/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
